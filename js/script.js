6ad56d720b177c119e7338619b7f9f51dd534b71
Andrii <dree0103@gmail.com>
2017-06-27 23:48:57 +0300

    done!

---
+++ js/script.js
@@ -0,0 +1,11 @@
+﻿$(window).load(function(){
+    var maxHeight = 0;
+    for( var i=0; i < $('.category-unit').length; i++) {
+        if ($('.category-unit').eq(i).height() > maxHeight) {
+            maxHeight = $('.category-unit').eq(i).height()
+        }
+    }
+    for( var i=0; i < $('.category-unit').length; i++) {
+        $('.category-unit').eq(i).css('height', maxHeight);
+    }
+});
